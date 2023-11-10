a92fd36b20c05eb917f54b8aa3ad604c5e29eb76
amirsho0017 <amirshorakhimov0017>
2023-11-10 09:43:13 +0500

    longest common prefix

---
+++ easy/longestCommonPrefix.py
@@ -0,0 +1,49 @@
+import os
+
+
+# first solution
+def common_prefix(strs):
+    if not strs:
+        return ""
+
+    prefix = strs[0]
+
+    for i in range(1, len(strs)):
+        current_string = strs[i]
+        j = 0
+
+        while j < min(len(prefix), len(current_string)) and prefix[j] == current_string[j]:
+            j += 1
+
+        prefix = prefix[:j]
+
+        if not prefix:
+            break
+
+    return prefix
+
+
+# Example usage:
+array_of_strings = ["flower", "flow", "flight"]
+
+
+result = common_prefix(array_of_strings)
+print(result)
+
+
+# second solution
+def common_prefix(strings):
+    if not strings:
+        return ""
+
+    # Using os.path.commonprefix to find the common prefix
+    prefix = os.path.commonprefix(strings)
+
+    return prefix
+
+
+# Example usage:
+array_of_strings = ["flower", "flow", "flight"]
+result = common_prefix(array_of_strings)
+print(result)
+
